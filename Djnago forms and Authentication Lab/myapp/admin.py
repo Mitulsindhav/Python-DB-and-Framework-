72f2f3fa989e671f79b36341ac5a297c2d6f6033
Mitul Sindhav <142717064+Mitulsindhav@users.noreply.github.com>
2025-04-17 17:48:44 +0530

    Django Forms and Authentication Lab

---
+++ Djnago forms and Authentication Lab/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Contact,User
+
+
+# Register your models here.
+
+admin.site.register(Contact) # admin ma show karva mate 
+admin.site.register(User) # admin ma show karva mate 
